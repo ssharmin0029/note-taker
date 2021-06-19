d9cba2e08f6c530c3e22d47f9f23373023b57474
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-06-19 09:43:40 -0400

    delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,6 @@ const path = require('path');
 const fs = require('fs');
 const {v4: uuidv4} = require('uuid');
 // const uuidv1 = require('uuid/v1');
-
 const dbPath = path.join(__dirname, '../db/db.json');
 
 router.get('/notes', (req, res) => {
@@ -25,13 +24,58 @@ router.post('/notes', (req, res) => {
     const newNote = {...req.body, id:uuidv4()};
     const updatedNotes = [...notes, newNote];
 
-    fs.writeFile(dbPath, JSON.stringify(updatedNotes), err => {
+    fs.writeFile(dbPath, JSON.stringify(updatedNotes), (err) => {
       if (err) {
         res.status(500).json(err);
       }
+      res.json(newNote);
     });
-    res.json(newNote);
+  });
+});
+
+router.delete('/notes/:id', (req, res) => {
+  const reqID = req.params.id;
+  console.log(reqID);
+
+  fs.readFile(dbPath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).res(err);
+    }
+    const notes = JSON.parse(data);
+    const newNotes = notes.filter(note => note.id !== reqID);
+    const filteredNotes = JSON.stringify(newNotes);
+
+    fs.writeFile(dbPath, filteredNotes, (err) => {
+      if (err) {
+        res.status(500).json(err);
+      }
+      // res.json({ok: true});
+    });
+    res.json({ok: true});
   });
 });
 
 module.exports = router;
+
+
+// router.delete('/notes/:id', (req, res) => {
+//   const reqID = req.params.id;
+//   console.log(reqID);
+
+//   fs.readFile(dbPath, 'utf8', (err, data) => {
+//     if (err) {
+//       res.status(500).res(err);
+//     }
+//     const notes = JSON.parse(data);
+//     const newNotes = notes.filter(note => note.id !== reqID);
+//     // const filteredNotes = JSON.stringify(newNotes);
+
+//     fs.writeFile(dbPath, JSON.stringify(newNotes), (err) => {
+//       if (err) {
+//         res.status(500).json(err);
+//       }
+//       // res.json({ok: true});
+//     });
+//     res.json({ok: true});
+//   });
+// });
